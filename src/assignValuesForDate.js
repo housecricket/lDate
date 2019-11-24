7f52793504e0c53288d5fe8ddb25d2848acf4ec9
trungtdd <duytrungdng@gmail.com>
2019-11-24 18:11:04 +0700

    - fix minor bug

--- src/assignValuesForDate.js
+++ src/assignValuesForDate.js
@@ -26,13 +26,13 @@ module.exports = function (lDate, sDate) {
     lDate.cMonth = getGanZhi.ofMonth(lDate.year, lDate.month);
     lDate.cYear = getGanZhi.ofYear(lDate.year);
 
-    lDate.goodHours = findGoodHoursInDay(ldate.cDay)
-    lDate.badHours = getEvilOfHourInDay(ldate.cDay)
+    lDate.goodHours = findGoodHoursInDay(lDate.cDay)
+    lDate.badHours = getEvilOfHourInDay(lDate.cDay)
     lDate.goodDay = isGoodDay(lDate.cMonth, lDate.cDay);
     lDate.mingNeiyin = getMingNeiyinOfGanZhi(lDate.cDay);
     lDate.element = getElementOfDay(lDate.cDay);
-    lDate.luckyDirection = getLuckyDirectionOfHour(ldate.cDay)
-    lDate.wealthDirection = getWealthDirectionOfHour(ldate.cDay)
+    lDate.luckyDirection = getLuckyDirectionOfHour(lDate.cDay)
+    lDate.wealthDirection = getWealthDirectionOfHour(lDate.cDay)
 
     lDate.officer = getOfficerOfDay(sDate.getUTCDate(), sDate.getUTCMonth() + 1, sDate.getUTCFullYear())
     lDate.getSolarTerm = getSolarTerm(sDate.getUTCDate(), sDate.getUTCMonth() + 1, sDate.getUTCFullYear())
